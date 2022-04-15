2bd02238ccb59adb332c6420d5faeb256f3f2948
PolinaShihel <polinashihel@campus.technion.ac.il>
2022-04-15 14:31:43 +0300

    finished function asciiArtPrintEncoded

--- AsciiArtTool.h
+++ AsciiArtTool.h
@@ -15,16 +15,20 @@
  */
 RLEList asciiArtRead(FILE* in_stream);
 
-
-
 /**
-* asciiArtPrintEncoded: Writes to file the picture in a compressed format.
-*
-* TThe compressed format corresponds to the type returned from the
-* RLEListExportString that's implemented in the RLEList.c file
-*
-* @return
-*
-*/
+ * asciiArtPrintEncoded: Writes to file the picture in a compressed format.
+ *
+ * The function prints out the compressed format corresponds to the type returned from the
+ * RLEListExportString that's implemented in the RLEList.c file into a file that was received as a pointer
+ *
+ * @param list - RLE list containing the ASCIIArt image in a RLEList struct
+ * @param out_stream  - FILE pointer to e file chosen to contain the compressed ascii image format
+ * @return
+ * 	RLE_LIST_NULL_ARGUMENT if a NULL was sent as one of the parameters
+ * 	RLE_LIST_OUT_OF_MEMORY if an allocation failed in the function used to create the compressed image
+ * 	RLE_LIST_SUCCESS if the compressed list was input into the file successfully
+ * 	RLE_LIST_ERROR if the file was opened with insufficient privileges
+ */
 RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream);
+
 #endif //ASCIIARTTOOL_H
