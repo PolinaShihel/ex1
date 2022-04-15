3b23f82c601987906e046d1c5397199b4c283e3e
PolinaShihel <polinashihel@campus.technion.ac.il>
2022-04-15 14:14:14 +0300

    finished function asciiArtPrintEncoded

--- AsciiArtTool.c
+++ AsciiArtTool.c
@@ -16,4 +16,21 @@ RLEList asciiArtRead(FILE* in_stream)
     return compressedImage;
 }
 
+RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream)
+{
+    if(!list||!out_stream){
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    RLEListResult errorCode;
+    char* compressedList = RLEListExportToString(list,&errorCode);
+    if(errorCode != RLE_LIST_SUCCESS){
+        return  errorCode;
+    }
+    if(0 > fprintf(out_stream, "%s", compressedList))
+        return RLE_LIST_ERROR; //if the file was opened with the wrong permissions that don't allow writing
+    free(compressedList);
+    return RLE_LIST_SUCCESS;
+}
+
+
 
