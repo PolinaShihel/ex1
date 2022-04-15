89fce57f82f47ca6b26a1ecec38631e4f48a0d26
oresh <oresh@ORESH>
2022-04-15 14:07:43 +0300

    Implemented asciiArtPrint & documented it in the .h file

--- AsciiArtTool.c
+++ AsciiArtTool.c
@@ -16,4 +16,31 @@ RLEList asciiArtRead(FILE* in_stream)
     return compressedImage;
 }
 
+RLEListResult asciiArtPrint(RLEList list, FILE* out_stream)
+{
+	if (!list || !out_stream) {
+		return RLE_LIST_NULL_ARGUMENT;
+	}
+
+	char* picture = malloc((RLEListSize(list) + 1) * sizeof(char));
+	if (!picture) {
+		return RLE_LIST_OUT_OF_MEMORY;
+	}
+
+	char* p = picture;
+	while (list)
+	{
+		while (list->letterAppearances--)
+		{
+			*p = list->letter;
+			p++;
+		}
+	}
+	*p = '\0';
+
+	int fputsResult = fputs(picture, out_stream) == EOF ? RLE_LIST_ERROR : RLE_LIST_SUCCESS;
+	free(picture);
+	return fputsResult;
+}
+
 
--- AsciiArtTool.h
+++ AsciiArtTool.h
@@ -4,6 +4,14 @@
 #include <stdio.h>
 
 /**
+  The following functions are available :
+* asciiArtRead - Creates an RLEList out of a file
+* asciiArtPrint: Writes the whole picture to the file stream using the
+* given list that represents the compressed picture
+* asciiArtPrintEncoded: Writes to file the picture in a compressed format.
+* /
+
+/**
  * asciiArtRead: creates an RLEList out of a file
  *
  * The function receives a FILE pointer and reads the contents of the file letter
@@ -15,12 +23,23 @@
  */
 RLEList asciiArtRead(FILE* in_stream);
 
-
+/**
+* asciiArtPrint: Writes the whole picture to the file stream using the
+* given list that represents the compressed picture
+*
+* @list - An RLEList that contains the file's characters
+* @out_stream - The stream that the picture will be written to
+* @return
+* 	RLE_LIST_NULL_ARGUMENT if a NULL was sent as one of the parameters
+* 	RLE_LIST_OUT_OF_MEMORY if a memory allocation has failed
+* 	RLE_LIST_SUCCESS if the picture has been written to the file successfully
+*/
+RLEListResult asciiArtPrint(RLEList list, FILE* out_stream);
 
 /**
 * asciiArtPrintEncoded: Writes to file the picture in a compressed format.
 *
-* TThe compressed format corresponds to the type returned from the
+* The compressed format corresponds to the type returned from the
 * RLEListExportString that's implemented in the RLEList.c file
 *
 * @return
