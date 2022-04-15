4102e8de227157b1324d009f396a7f9c37350cf0
PolinaShihel <polinashihel@campus.technion.ac.il>
2022-04-15 12:37:01 +0300

    Created files

--- AsciiArtTool.h
+++ AsciiArtTool.h
@@ -1,4 +1,17 @@
-#ifndef EX1_ASCIIARTTOOL_H
-#define EX1_ASCIIARTTOOL_H
+#ifndef ASCIIARTTOOL_H
+#define ASCIIARTTOOL_H
 
-#endif //EX1_ASCIIARTTOOL_H
+#include <stdio.h>
+
+
+/**
+* asciiArtPrintEncoded: Writes to file the picture in a compressed format.
+*
+* TThe compressed format corresponds to the type returned from the
+* RLEListExportString that's implemented in the RLEList.c file
+*
+* @return
+*
+*/
+RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream);
+#endif //ASCIIARTTOOL_H
