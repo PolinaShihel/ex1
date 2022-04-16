30757d94a38ca5b723ae31b44838a4c356c8fcf3
oresh <oresh@ORESH>
2022-04-16 23:17:24 +0300

    * Fixed asciiArtPrint (forgot to put list->next in list before the next iteration of the while loop). * Fixed some run-time errors.

--- AsciiArtTool.c
+++ AsciiArtTool.c
@@ -1,16 +1,17 @@
 #include "AsciiArtTool.h"
 #include "RLEList.h"
+
 RLEList asciiArtRead(FILE* in_stream)
 {
-    if(!in_stream){//instructed to return null if the file pointer is invalid
+    if (!in_stream) {//instructed to return null if the file pointer is invalid
         return NULL;
     }
     char currentLetter;
     RLEList compressedImage = RLEListCreate();
-    do{
+    do {
         currentLetter = fgetc(in_stream);
-        RLEListAppend(compressedImage,currentLetter);
-    }while (currentLetter != EOF);
+        RLEListAppend(compressedImage, currentLetter);
+    } while (currentLetter != EOF);
     return compressedImage;
 }
 
@@ -25,35 +26,35 @@ RLEListResult asciiArtPrint(RLEList list, FILE* out_stream)
         return RLE_LIST_OUT_OF_MEMORY;
     }
 
-    char* p = picture;
-    while (list)
-    {
-        while (list->letterAppearances--)
-        {
-            *p = list->letter;
-            p++;
+    char* picturePointer = picture;
+    while (list) {
+        while (list->letterAppearances--) {
+            *picturePointer = list->letter;
+            picturePointer++;
         }
+        list = list->next;
     }
-    *p = '\0';
+    *picturePointer = '\0';
 
     int fputsResult = fputs(picture, out_stream) == EOF ? RLE_LIST_ERROR : RLE_LIST_SUCCESS;
     free(picture);
     return fputsResult;
 }
 
-RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream)
+RLEListResult asciiArtPrintEncoded(RLEList list, FILE* out_stream)
 {
-    if(!list||!out_stream){
+    if (!list || !out_stream) {
         return RLE_LIST_NULL_ARGUMENT;
     }
     RLEListResult errorCode;
-    char* compressedImage = RLEListExportToString(list,&errorCode);
-    if(!(errorCode = RLE_LIST_SUCCESS))
-    {
+    char* compressedImage = RLEListExportToString(list, &errorCode);
+    if (errorCode != RLE_LIST_SUCCESS) {
         return errorCode;
     }
-    fprintf(out_stream,"%s",compressedImage);
+    fprintf(out_stream, "%s", compressedImage);
     free(compressedImage);
     return RLE_LIST_SUCCESS;
 }
+
+
 
