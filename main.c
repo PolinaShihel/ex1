d507df3d717d04f8f58769c8f13beeafc8e818c0
oresh <oresh@ORESH>
2022-04-16 23:13:13 +0300

    * Fixed invertedRLEList according to the hw requirements * Fixed some compilation & run-time errors

--- main.c
+++ main.c
@@ -1,50 +1,55 @@
-#include <stdio.h>
-#include "RLEList.h"
 #include "AsciiArtTool.h"
 
 #define EXPECTED_ARGUMENTS_COUNT 3
 
-#define ENCODED_FLAG -e
-#define INVERTED_FLAG -i
+#define ENCODED_FLAG "-e"
+#define INVERTED_FLAG "-i"
 
 #define FLAG_ARG_INDEX 0
 #define SOURCE_ARG_INDEX 1
 #define TARGET_ARG_INDEX 2
 
 #define FILE_ERROR -1
-#define LETTER_TO_INVERT ' '
-#define LETTER_INVERTED '@'
+#define SPACE_CHARACTER ' '
+#define AT_CHARACTER '@'
 
-char invertedRLEList(char originalLetter)
-{
-    if(!originalLetter){
-        return RLE_LIST_NULL_ARGUMENT;
+char invertedRLEList(char originalLetter) {
+    if (!originalLetter) {
+        return '\0';
     }
-    if(originalLetter== LETTER_TO_INVERT){
-        return LETTER_INVERTED;
+
+    switch (originalLetter) {
+        case AT_CHARACTER: {
+            return SPACE_CHARACTER;
+        }
+        case SPACE_CHARACTER: {
+            return AT_CHARACTER;
+        }
+        default: {
+            return originalLetter;
+        }
     }
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     if (argc == EXPECTED_ARGUMENTS_COUNT) {
         FILE* sourceFileStream = fopen(argv[SOURCE_ARG_INDEX], "%r");
         FILE* targetFileStream = fopen(argv[TARGET_ARG_INDEX], "%w");
         RLEList orignialArt = asciiArtRead(sourceFileStream);
-        if(!orignialArt){
+        if (!orignialArt) {
             return FILE_ERROR;
         }
+
         if (argv[FLAG_ARG_INDEX] == ENCODED_FLAG) {
-            if(asciiArtPrint(orignialArt, targetFileStream)!=RLE_LIST_SUCCESS){
+            if (asciiArtPrint(orignialArt, targetFileStream) != RLE_LIST_SUCCESS) {
                 return NULL;
             }
         }
-        else if (argv[FLAG_ARG_INDEX == INVERTED_FLAG]) {
-            MapFunction invertedASCIIArt = &invertedRLEList;
-            RLEListMap(orignialArt,invertedASCIIArt);
-            asciiArtPrintEncoded(orignialArt,targetFileStream);
+        else if (argv[FLAG_ARG_INDEX] == INVERTED_FLAG) {
+            RLEListMap(orignialArt, invertedRLEList);
+            asciiArtPrintEncoded(orignialArt, targetFileStream);
         }
-        else{//if the provided parameter is not fitting either options
+        else {//if the provided parameter is not fitting either options
             fclose(sourceFileStream);
             fclose(targetFileStream);
             return NULL;
@@ -52,7 +57,7 @@ int main(int argc, char** argv)
         fclose(sourceFileStream);
         fclose(targetFileStream);
     }
-    else{//if the number of arguments provided is not as expected
+    else {//if the number of arguments provided is not as expected
         return NULL;
     }
     return 0;
